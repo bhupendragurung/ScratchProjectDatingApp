42476ca928488ab15e64cee0f6bc5286cac9f05a
BhuGrg <bhupendragurung263@gmail.com>
2023-02-18 00:30:51 +0000

    changing gitignore filtest  vv

--- ScratchProjectDatingApp/Entity/AppUser.cs
+++ ScratchProjectDatingApp/Entity/AppUser.cs
@@ -2,6 +2,9 @@
 {
     public class AppUser
     {
+        /// <summary>
+        /// for key id
+        /// </summary>
         public int Id { get; set; }
         public string UserName { get; set; }
     }
